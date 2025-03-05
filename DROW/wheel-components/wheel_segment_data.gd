252f67efbe56fe32e13492620e0696480d61c0ab
Parth Patel <parthpatel6400@gmail.com>
2025-03-04 20:05:15 -0800

    deleting DROW"

--- DROW/wheel-components/wheel_segment_data.gd
+++
@@ -1,9 +0,0 @@
-class_name WheelSegmentData
-extends Resource
-
-## Segment color acts like a color filter, not additive light.
-@export var color:Color = Color.WHITE
-## Base value for this segment.
-@export var value:int = 1
-## Whether or not this segment is selectable (if false, displays the cover for this segment).
-@export var selectable:bool = true
